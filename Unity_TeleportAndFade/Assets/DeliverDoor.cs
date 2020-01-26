8997ef5077ea55b9b48e9ca395f7e8756f77e1b8
GuavaX <57407388+GuavaX@users.noreply.github.com>
2020-01-26 12:50:44 +0800

    作業10_載入關卡與轉場效果_完成

---
+++ Unity_TeleportAndFade/Assets/DeliverDoor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class DeliverDoor : MonoBehaviour
+{
+    private Image imgCross;
+
+    private void Start()
+    {
+        imgCross = GameObject.Find("轉場效果").GetComponent<Image>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //print(other.name);
+        if (other.name == "傳送區域")
+        {
+            StartCoroutine(NextLevel());
+        }
+    }
+
+    private IEnumerator NextLevel()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            imgCross.color += new Color(0, 0, 0, 0.02f);
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        //yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene("第二關");
+        
+    }
+
+}
---
+++ Unity_TeleportAndFade/Assets/Level2GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Level2GameManager : MonoBehaviour
+{
+    private Image imgCross;
+
+    private void Start()
+    {
+        imgCross = GameObject.Find("轉場效果").GetComponent<Image>();
+        imgCross.color += new Color(0, 0, 0, 1); //透明度範圍為 0~1
+
+        StartCoroutine(EnterLevel()); //載入關卡時執行淡入效果
+    }
+
+    private IEnumerator EnterLevel() //淡入效果
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            imgCross.color -= new Color(0, 0, 0, 0.02f);
+            yield return new WaitForSeconds(0.05f);
+        }
+
+    }
+
+}
