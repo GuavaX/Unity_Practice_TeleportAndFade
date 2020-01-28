a92e20e653e52f6b33c531ef4798117a09c8cdfe
GuavaX <57407388+GuavaX@users.noreply.github.com>
2020-01-29 00:05:05 +0800

    新增黑道

---
+++ Unity_TeleportAndFade/Assets/Bandit.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Bandit : MonoBehaviour
+{
+    private Transform target;
+    private NavMeshAgent nav;
+    private Animator ani;
+
+    private float stopDistance = 2;
+
+    private void Start()
+    {
+        target = GameObject.Find("玩家").GetComponent<Transform>();
+        nav = GetComponent<NavMeshAgent>();
+        ani = GetComponent<Animator>();
+
+        nav.stoppingDistance = stopDistance;
+
+        //ani.SetBool("Walk", true);        
+        ani.SetBool("Run", true);        
+    }
+
+    private void Update()
+    {
+        nav.SetDestination(target.position);
+        transform.LookAt(target.position);
+
+        if (nav.remainingDistance < stopDistance)
+        {
+            //ani.SetBool("Walk", false);
+            ani.SetBool("Run", false);
+            ani.SetTrigger("kick");
+        }
+        else
+        {
+            //ani.SetBool("Walk", true);
+            ani.SetBool("Run", true);
+        }
+    }
+
+
+}
